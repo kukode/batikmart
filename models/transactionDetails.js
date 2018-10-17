f4b304004bad01f4e69afbdfa3d9decd380be020
adirahman <kodingku@github.com>
2018-10-17 19:25:35 +0700

    update transaction details models

--- models/transactionDetails.js
+++ models/transactionDetails.js
@@ -1,13 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const transactionDetails = sequelize.define('transactionDetails', {
-    // transId: DataTypes.INTEGER,
+    transId: DataTypes.INTEGER,
     price: DataTypes.STRING,
     quantity: DataTypes.STRING,
     name: DataTypes.STRING
   }, {});
   transactionDetails.associate = function(models) {
     // associations can be defined here
+    transactionDetails.belongsTo(models.transaction)
   };
   return transactionDetails;
 };
