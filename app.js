d11d1ff538ac21254f9cb14d6226b41f292e9acc
adirahman <kodingku@github.com>
2018-10-23 22:35:57 +0700

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize())
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'gambar')));
+app.use(express.static(path.join(__dirname, 'public/uploads')));
 
 
 app.use('/api', indexRouter);
